a4009a3a9fcf249890d4c2bb0de4a95b87b44b01
Jason Diaz <48036849+Jason-Diaz@users.noreply.github.com>
2019-06-27 11:13:53 -0400

    Add files via upload

---
+++ Lab-Arrays/Lab-Arrays/Grade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Arrays
+{
+    class Grade : IComparable<Grade>
+    {
+        public double score { get; set; }
+        public string description { get; set; }
+
+        public Grade(double v1, string v2)
+        {
+            score = v1;
+            description = v2;
+        }
+
+       
+
+        public int CompareTo(Grade obj)
+        {
+            if(score < obj.score)
+            {
+                return -1;
+            }
+
+            if(score > obj.score)
+            {
+                return 1;
+            }
+
+            return 0;
+        
+        }
+
+        public string toString()
+        {
+            return "score: " + score + " description: " + description;
+        }
+    }
+}
---
+++ Lab-Arrays/Lab-Arrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Arrays
+{
+    class Program
+    {
+        static void Display(double[] arr)
+        {
+            foreach (double i in arr)
+            {
+                
+                System.Console.Write(i + " ");
+               
+            }
+            System.Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            double[] Num = new double[4] { 4.4, 7.7, 1, 2.3 };
+            Display(Num);
+
+            Array.Sort(Num);
+            Display(Num);
+
+            double[] Clone = (double[])Num.Clone();
+            Display(Clone);
+
+            Grade[] Grades = new Grade[4];
+            Grades[0] = new Grade(50.5,"Test");
+            Grades[1] = new Grade(70.5, "Quiz");
+            Grades[2] = new Grade(80.6, "Lab");
+            Grades[3] = new Grade(40.3, "Part");
+            
+            foreach (Grade i in Grades)
+            {
+                string s = i.toString();
+                System.Console.WriteLine(s);
+            }
+            Array.Sort(Grades);
+
+            System.Console.WriteLine();
+
+            foreach (Grade i in Grades)
+            {
+                string s = i.toString();
+                System.Console.WriteLine(s);
+            }
+        }
+    }
+}
